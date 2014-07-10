8be95b5aeab623f8b83350c4c9180b56f9f799be
Hzj-jie <Hzj_jie@hotmail.com>
2014-07-10 17:13:48 +0800

    original design of auto_removed_folder cannot work with cl

--- utils/auto_removed_folder.hpp
+++ utils/auto_removed_folder.hpp
@@ -1,29 +1,20 @@
 
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wreorder"
-
 #include "sweeper.hpp"
 #include <boost/filesystem.hpp>
 
 class auto_removed_folder : private sweeper
 {
-private:
-    const boost::filesystem::path p;
 public:
 #define AUTO_REMOVED_FOLDER_CTOR \
-            p(p), \
             sweeper([&]() { \
                         boost::filesystem::create_directory(p); }, \
-                    [this]() { \
-                        boost::filesystem::remove_all(this->p); }) { }
+                    [=]() { \
+                        boost::filesystem::remove_all(p); }) { }
     auto_removed_folder(boost::filesystem::path&& p) :
         AUTO_REMOVED_FOLDER_CTOR;
     auto_removed_folder(const boost::filesystem::path& p) :
         AUTO_REMOVED_FOLDER_CTOR;
 #undef AUTO_REMOVED_FOLDER_CTOR
 };
-
-#pragma GCC diagnostic pop
-
