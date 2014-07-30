85cd25f946f005535c411e62abfd5439e37be9eb
hzj-jie <hzj_jie@hotmail.com>
2014-07-30 16:38:49 +0800

    threadpool

---
+++ threadpool/threadpool.hpp
@@ -0,0 +1,15 @@
+
+#pragma once
+#include "../const/threadpool.hpp"
+#include <stdint.h>
+
+// TODO: threadpool
+
+class threadpool
+{
+protected:
+    static uint32_t default_thread_count()
+    {
+    }
+};
+
