66caeb8b13281137b002b8c67d1d17f16d6cb06f
Hzj-jie <hzj_jie@hotmail.com>
2014-08-11 13:36:05 +0800

    update to boost 1.56

--- utt/compile.bat
+++ utt/compile.bat
@@ -1,3 +1,3 @@
 
-cl /Ox main.cpp /Feutt /I c:\Boost\include\boost-1_55 /MD /EHsc /D_WIN32_WINNT=0x501 /link /LIBPATH:c:\Boost\lib\ /SUBSYSTEM:CONSOLE,5.01
+cl /Ox main.cpp /Feutt /I c:\Boost\include\boost-1_56 /MD /EHsc /D_WIN32_WINNT=0x501 /link /LIBPATH:c:\Boost\lib\ /SUBSYSTEM:CONSOLE,5.01
 
