22bdde1945dee64210f7092c768950b89102b581
Hzj-jie <hzj_jie@hotmail.com>
2014-08-14 13:10:39 +0800

    update timing_counter_test

--- utt_cases/utils/timing_counter_test.hpp
+++ utt_cases/utils/timing_counter_test.hpp
@@ -4,11 +4,13 @@
 #include "../../utils/timing_counter.hpp"
 #include "../../utt/icase.hpp"
 #include "../../utt/utt_assert.hpp"
+#include "../../utt/case_wrapper/multithreading_case_wrapper.hpp"
+#include "../../utt/case_wrapper/repeat_case_wrapper.hpp"
 
-class timing_counter_test : public icase
+class timing_counter_case : public icase
 {
 public:
-    bool run() override
+    bool execute() override
     {
         const uint32_t wait_ms = 100;
         int64_t ms;
@@ -21,8 +23,17 @@ public:
         return true;
     }
 
-    DEFINE_CASE(timing_counter_test);
+    DEFINE_CASE(timing_counter_case);
 };
 
+class timing_counter_test : public
+          multithreading_case_wrapper<
+              repeat_case_wrapper<
+                  timing_counter_case,
+                  1024>,
+              8>
+{
+    DEFINE_CASE(timing_counter_test);
+};
 REGISTER_CASE(timing_counter_test);
 
