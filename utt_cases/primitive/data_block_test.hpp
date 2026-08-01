3bed4c16db3614ec205fcc9ab934d78d4313fd62
zijiehe-google-com <zijiehe@google.com>
2026-07-31 22:12:04 -0700

    Split primitive tests into granular test cases

--- utt_cases/primitive/data_block_test.hpp
+++ utt_cases/primitive/data_block_test.hpp
@@ -3,33 +3,70 @@
 #include "../../utt/icase.hpp"
 #include "../../utt/utt_assert.hpp"
 
-class data_block_test : public icase
+namespace primitive_test
 {
-public:
-    bool run() override
+    class data_block_int_test : public icase
     {
-        using namespace primitive;
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_block b_int = data_block::from_int32(12345);
+            utt_assert.equal(b_int.as_int32(), 12345);
+            utt_assert.equal(b_int.as_int64(), static_cast<int64_t>(12345));
+            return true;
+        }
 
-        data_block b_int = data_block::from_int32(12345);
-        utt_assert.equal(b_int.as_int32(), 12345);
-        utt_assert.equal(b_int.as_int64(), static_cast<int64_t>(12345));
+        DEFINE_CASE(data_block_int_test);
+    };
 
-        data_block b_long = data_block::from_int64(9876543210LL);
-        utt_assert.equal(b_long.as_int64(), 9876543210LL);
+    class data_block_long_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_block b_long = data_block::from_int64(9876543210LL);
+            utt_assert.equal(b_long.as_int64(), 9876543210LL);
+            return true;
+        }
 
-        data_block b_bool_t = data_block::from_bool(true);
-        utt_assert.is_true(b_bool_t.as_bool());
+        DEFINE_CASE(data_block_long_test);
+    };
 
-        data_block b_bool_f = data_block::from_bool(false);
-        utt_assert.is_false(b_bool_f.as_bool());
+    class data_block_bool_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_block b_bool_t = data_block::from_bool(true);
+            utt_assert.is_true(b_bool_t.as_bool());
 
-        data_block b_str = data_block::from_string("hello_primitive");
-        utt_assert.equal(b_str.as_string(), std::string("hello_primitive"));
+            data_block b_bool_f = data_block::from_bool(false);
+            utt_assert.is_false(b_bool_f.as_bool());
+            return true;
+        }
 
-        return true;
-    }
+        DEFINE_CASE(data_block_bool_test);
+    };
 
-    DEFINE_CASE(data_block_test);
-};
+    class data_block_string_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_block b_str = data_block::from_string("hello_primitive");
+            utt_assert.equal(b_str.as_string(), std::string("hello_primitive"));
+            return true;
+        }
 
-REGISTER_CASE(data_block_test);
+        DEFINE_CASE(data_block_string_test);
+    };
+}
+
+REGISTER_CASE(primitive_test::data_block_int_test);
+REGISTER_CASE(primitive_test::data_block_long_test);
+REGISTER_CASE(primitive_test::data_block_bool_test);
+REGISTER_CASE(primitive_test::data_block_string_test);
--- utt_cases/primitive/data_ref_test.hpp
+++ utt_cases/primitive/data_ref_test.hpp
@@ -3,50 +3,90 @@
 #include "../../utt/icase.hpp"
 #include "../../utt/utt_assert.hpp"
 
-class data_ref_test : public icase
+namespace primitive_test
 {
-public:
-    bool run() override
+    class data_ref_abs_test : public icase
     {
-        using namespace primitive;
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_ref r = data_ref::abs(42);
+            utt_assert.equal(static_cast<uint8_t>(r.type), static_cast<uint8_t>(ref_type::abs));
+            utt_assert.equal(r.offset, 42);
 
-        data_ref r1 = data_ref::abs(42);
-        utt_assert.equal(static_cast<uint8_t>(r1.type), static_cast<uint8_t>(ref_type::abs));
-        utt_assert.equal(r1.offset, 42);
+            int64_t encoded = r.encode();
+            data_ref parsed;
+            utt_assert.is_true(data_ref::parse(encoded, parsed));
+            utt_assert.equal(parsed, r);
+            return true;
+        }
 
-        int64_t encoded = r1.encode();
-        data_ref parsed;
-        utt_assert.is_true(data_ref::parse(encoded, parsed));
-        utt_assert.equal(parsed, r1);
+        DEFINE_CASE(data_ref_abs_test);
+    };
 
-        data_ref r2 = data_ref::rel(-10);
-        utt_assert.equal(static_cast<uint8_t>(r2.type), static_cast<uint8_t>(ref_type::rel));
-        utt_assert.equal(r2.offset, -10);
+    class data_ref_rel_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_ref r = data_ref::rel(-10);
+            utt_assert.equal(static_cast<uint8_t>(r.type), static_cast<uint8_t>(ref_type::rel));
+            utt_assert.equal(r.offset, -10);
 
-        encoded = r2.encode();
-        utt_assert.is_true(data_ref::parse(encoded, parsed));
-        utt_assert.equal(parsed, r2);
+            int64_t encoded = r.encode();
+            data_ref parsed;
+            utt_assert.is_true(data_ref::parse(encoded, parsed));
+            utt_assert.equal(parsed, r);
+            return true;
+        }
 
-        data_ref r3 = data_ref::habs(100);
-        utt_assert.equal(static_cast<uint8_t>(r3.type), static_cast<uint8_t>(ref_type::habs));
-        utt_assert.equal(r3.offset, 100);
+        DEFINE_CASE(data_ref_rel_test);
+    };
 
-        encoded = r3.encode();
-        utt_assert.is_true(data_ref::parse(encoded, parsed));
-        utt_assert.equal(parsed, r3);
+    class data_ref_habs_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_ref r = data_ref::habs(100);
+            utt_assert.equal(static_cast<uint8_t>(r.type), static_cast<uint8_t>(ref_type::habs));
+            utt_assert.equal(r.offset, 100);
 
-        data_ref r4 = data_ref::hrel(-5);
-        utt_assert.equal(static_cast<uint8_t>(r4.type), static_cast<uint8_t>(ref_type::hrel));
-        utt_assert.equal(r4.offset, -5);
+            int64_t encoded = r.encode();
+            data_ref parsed;
+            utt_assert.is_true(data_ref::parse(encoded, parsed));
+            utt_assert.equal(parsed, r);
+            return true;
+        }
 
-        encoded = r4.encode();
-        utt_assert.is_true(data_ref::parse(encoded, parsed));
-        utt_assert.equal(parsed, r4);
+        DEFINE_CASE(data_ref_habs_test);
+    };
 
-        return true;
-    }
+    class data_ref_hrel_test : public icase
+    {
+    public:
+        bool run() override
+        {
+            using namespace primitive;
+            data_ref r = data_ref::hrel(-5);
+            utt_assert.equal(static_cast<uint8_t>(r.type), static_cast<uint8_t>(ref_type::hrel));
+            utt_assert.equal(r.offset, -5);
 
-    DEFINE_CASE(data_ref_test);
-};
+            int64_t encoded = r.encode();
+            data_ref parsed;
+            utt_assert.is_true(data_ref::parse(encoded, parsed));
+            utt_assert.equal(parsed, r);
+            return true;
+        }
 
-REGISTER_CASE(data_ref_test);
+        DEFINE_CASE(data_ref_hrel_test);
+    };
+}
+
+REGISTER_CASE(primitive_test::data_ref_abs_test);
+REGISTER_CASE(primitive_test::data_ref_rel_test);
+REGISTER_CASE(primitive_test::data_ref_habs_test);
+REGISTER_CASE(primitive_test::data_ref_hrel_test);
